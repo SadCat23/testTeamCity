30014b3d0371f65e5fc8e5f576d92bc4c2725c26
SadCat23 <davidberzon19982301@gmail.com>
2021-11-28 17:28:34 +0300

    test

---
+++ MyProject/build.bat
@@ -0,0 +1,7 @@
+"G:/UE_4.27/Engine/Build/BatchFiles/RunUAT.bat" ^
+BuildCookRun ^
+-project=Y:/testTeamCity/MyProject/MyProject.uproject ^
+-platform=Win64 ^
+-clientconfig=Development ^
+-archivedirectory=Y:/testTeamCity/MyProject/Build ^
+-cook -stage -archive -pak -package -allmaps ^
